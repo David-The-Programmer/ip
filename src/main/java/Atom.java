558660f0c2fd036e80804427bbd4bd3c250e40aa
David <42868389+David-The-Programmer@users.noreply.github.com>
2026-01-17 13:37:24 +0800

    Complete Level 0: Rename, Greet, Exit

---
+++ src/main/java/Atom.java
@@ -0,0 +1,16 @@
+public class Atom {
+    public static void main(String[] args) {
+        String logo = 
+        "          :::        :::::::::::    ::::::::          :::   ::: \n" +
+        "       :+: :+:          :+:       :+:    :+:        :+:+: :+:+: \n" +
+        "     +:+   +:+         +:+       +:+    +:+       +:+ +:+:+ +:+ \n" +
+        "   +#++:++#++:        +#+       +#+    +:+       +#+  +:+  +#+  \n" +
+        "  +#+     +#+        +#+       +#+    +#+       +#+       +#+   \n" +
+        " #+#     #+# #+#    #+# #+#   #+#    #+# #+#   #+#       #+# #+#\n" +
+        "###     ### ###    ### ###    ########  ###   ###       ###  ###\n";
+        System.out.println(logo);
+        System.out.println("Hello, I am an Assistive Task Organisation Manager, or A.T.O.M.");
+        System.out.println("How can I help you?\n");
+        System.out.println("Goodbye! Exiting...");
+    }
+}
